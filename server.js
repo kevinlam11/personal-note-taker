0925aa1b77612d60957a29c59692fb1f0cb0f993
kevnviek303@yahoo.com <kevnivek303@yahoo.com>
2022-03-25 14:38:06 -0400

    updated js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const fs = 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -7,10 +8,21 @@ const PORT = process.env.PORT || 3001;
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.get("*", (req, res) => {
+app.get("/index", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+// app.get("/api/notes", (req, res) => res.json(db.json));
+// app.post("/api/notes", (req, res) => res.json(db.json));
+
+
+
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
