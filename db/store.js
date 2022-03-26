b20a94e48dc95da6e21b82e3b39fd0a5ee67a666
kevnviek303@yahoo.com <kevnivek303@yahoo.com>
2022-03-26 00:04:50 -0400

    completed get and post

--- db/store.js
+++ db/store.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const util = require("util");
+const { v4: uuidv4 } = require("uuid");
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
@@ -9,14 +10,28 @@ class Store {
     return readFileAsync("db/db.json", "utf-8");
   }
 
-  writeNote(note) {
-    return readFileAsync("db/db.json", JSON.stringify(note));
+  writeNote(notes) {
+    return writeFileAsync("db/db.json", JSON.stringify(notes));
   }
 
   getNotes() {
     return this.readNotes().then((result) => {
-      return JSON.parse(result);
+      let parsedNotes;
+      try {
+        parsedNotes = [...JSON.parse(result)];
+      } catch (err) {
+        parsedNotes = [];
+      }
+      return parsedNotes;
     });
+  }
+
+  addNote(note) {
+    const newNote = { id: uuidv4(), title: note.title, text: note.text };
+    return this.getNotes()
+      .then((notes) => [...notes, newNote])
+      .then((updatedNotes) => this.writeNote(updatedNotes))
+      .then(() => newNote);
   }
 }
 
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,4 +12,11 @@ router.get("/notes", (req, res) => {
     });
 });
 
+router.post("/notes", (req, res) => {
+  store
+    .addNote(req.body)
+    .then((note) => res.json(note))
+    .catch((err) => res.status(500).json(err));
+});
+
 module.exports = router;
